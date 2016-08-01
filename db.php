32dd21f57056dec20118d1f304ab22bf74166080
jyllson <jyllson@gmail.com>
2016-08-01 22:15:36 +0200

    Add files via upload

---
+++ db.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "mysql334.loopia.se";
+$username = "petar@l31495";
+$password = "nefertiti1205";
+$dbname = "lifedesign_rs_db_3";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->exec('SET NAMES utf8');
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+	
+	
---
+++ head.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="sr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>M-Burger | Prijem Porudžbina</title>
+
+    <meta name="description" content="Bio jednom jedan burger. Kakav burger? Strašan burger!">
+    <meta name="author" content="M-Burger">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+  </head>
